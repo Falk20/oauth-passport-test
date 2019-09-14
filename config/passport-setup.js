b0f1882d50492a596edcf323ce35876b2294e298
falk <petruk0908@gmail.com>
2019-09-14 23:37:02 +0300

    added user to mongodb

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const secret = require('./client_secret.json');
+const User = require('../models/user-model');
 
 passport.use(
     new GoogleStrategy({
@@ -12,6 +13,11 @@ passport.use(
         //pass callback
         console.log('pass callback fired');
         console.log(profile);
-        done(profile);
+        new User({
+            userName: profile.displayName,
+            googleID: profile.id
+        }).save().then((newUser)=>{
+            console.log('new user created:', newUser);
+        });
     })
 );
